0f62783087c667ce5348518447ce3271d8b83a9f
侯强 <houqiang@findingunicornart.com>
2021-08-13 18:15:32 +0800

    new：工具类型和类型物料

--- 14.ts
+++ 14.ts
@@ -2,7 +2,7 @@
  * @Author: mellow
  * @Date: 2021-08-06 15:45:09
  * @LastEditors: mellow
- * @LastEditTime: 2021-08-06 18:31:17
+ * @LastEditTime: 2021-08-11 16:24:23
  */
 {
 
@@ -10,6 +10,10 @@
   interface Mellow {
     name:string
     age:number
+    todo?:string
+  }
+  interface Mellow2 {
+    name:string
     todo?:string
   }
 
@@ -44,10 +48,40 @@
   // readonlyMellow.name = 'miniyellow'
 
 
+  type Omit =  Pick<Mellow, Exclude<keyof Mellow, 'todo'>>
+
+  // keyof 获得的是接口属性的联合类型
+  type Key = keyof Mellow  // 'age' | 'name' | 'todo'
+
+  // 若T为联合类型则会分配式计算
+  type diff = Exclude<'age' | 'name' | 'todo','todo'>
 
 
+  // 交集
+  type Extract<T,U> = T extends U ? T : never
+
+  type And = Extract<keyof Mellow,'name'>
+
+  // type和interface都可以表示对象类型
+  type In<T,U> = {
+    [K in Extract<keyof T,keyof U>] : T[K]
+  }
+
+  type AA = In<Mellow,Mellow2>
+
+  interface NoNull {
+    name:string
+    age:null
+    todo?:string
+    kk:undefined
+  }
 
 
+  // 从   联合类型  中去除 null 或者 undefined 的类型
+  type YY = NonNullable<string|number|null|undefined>
+
+  // 在 TypeScript 中，keyof any 指代可以作为对象键的属性;目前，JavaScript 仅支持string、number、symbol作为对象的键值。
+  type AllKey = keyof any  // string、number、symbol
 
 
 
---
+++ 15.ts
@@ -0,0 +1,142 @@
+/*
+ * @Author: mellow
+ * @Date: 2021-08-10 16:33:36
+ * @LastEditors: mellow
+ * @LastEditTime: 2021-08-12 17:32:59
+ */
+{
+
+  type IsExtends<T> = T extends {name:infer N,age:infer A} ? [N,A] : number
+
+  type Num = IsExtends<{name:'mellow'}>
+
+
+  class Person {
+    constructor(name: string, age?: number) {}
+  }
+  console.log(typeof Person);
+
+  interface MixedObject {
+    animal:{
+      type:'animal' | 'cat' | 'dog'
+      age:number
+    }
+    [name:number]:{
+      type:string
+      age:number
+      nickname:string
+    }
+    [name:string]:{
+      type:string
+      age:number
+    }
+  }
+
+  type animal = MixedObject['animal']
+  type animalType = MixedObject['animal']['type']
+  type numberIndex = MixedObject[number]
+  type numberIndex12 = MixedObject[12]
+  type stringIndex = MixedObject[string]
+  type stringIndex12 = MixedObject['12']
+
+  type Mixkey = keyof MixedObject
+
+
+  let a = 'aaaaaa'
+
+  let b = typeof a
+
+  let c: typeof a = 'c'
+
+  // 只是用extends是对类型进行约束，不符合约束类型会直接报错    
+  type TargetGeneric<T extends string | number> = {
+    [K in T] : string
+  }
+
+  type TargetT = TargetGeneric<'id'|12>
+
+
+  // !==========================================================================
+
+
+
+  interface StringIndexArray {
+    [index:string] : string
+  }
+
+  type k1 = keyof StringIndexArray
+
+  type k2 = StringIndexArray[0]
+
+
+  function prop<T,K extends keyof T>(obj:T,k:K){
+    return obj[k]
+  }
+  let mellow = {
+    name:'mellow',
+    age:1
+  }
+  prop(mellow,'name')
+  prop(mellow,'age')
+
+
+
+  // ! 泛型
+  type isSubTyping<C,P> = C extends P ? true : false 
+  type s1 = isSubTyping<1,number>
+  type s2 = isSubTyping<number,1>
+
+  // ! 条件类型
+  type isAssertable<T,S> = T extends S ? true : S extends T ? true : false
+  type a1 = isAssertable<1,number>
+  type a2 = isAssertable<1,boolean>
+
+  // ! 分配条件类型（分配式计算）
+  type BooleanOrString = string | boolean
+  type StringOrNumberArray<E> = E extends string | number ? E[] : E
+
+  // 此时BooleanOrString会根据分配原则进行
+  type WhatIsThis = StringOrNumberArray<BooleanOrString> // boolean | string[] 
+
+  // 此时BooleanOrString作为一个整体和string | number比较
+  type BooleanOrStringGot = BooleanOrString extends string | number ? BooleanOrString[] : BooleanOrString // string | boolean
+
+  // 若使用泛型取整体要用[]给括起来
+  type StringOrNumberArrayAll<E> = [E] extends [string | number] ? E[] : E
+  type WhatIsThis2 = StringOrNumberArrayAll<BooleanOrString>
+
+  // never类型陷阱, 
+  // 1、never在泛型若以原子入参，则为false
+  // 2、never在泛型若以整体入参，因为never 是所有类型的子类型所以为true
+  type GetSNums = never extends number ? number[] : never extends string ? string[] : never  // number[]
+  type GetNeverArray = StringOrNumberArrayAll<never>
+  type GetNever = StringOrNumberArray<never>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
